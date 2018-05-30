a67b14d1b8946038eb46d0ba037f55873f87fc73
a.vernay <alexisvernay38@gmail.com>
2018-05-30 10:41:43 +0200

    Rajout de la classe jourFerie

--- src/main/java/dev/entities/CongeEnum.java
+++ src/main/java/dev/entities/CongeEnum.java
@@ -1,19 +1,48 @@
 package dev.entities;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Enum CongeEnum.
+ */
 public enum CongeEnum {
 
-	CONGE_PAYE("Congé payé"), RTT("RTT"), CONGE_SANS_SOLDE("Congé sans solde"), MISSION("Mission");
+	/** The conge paye. */
+	CONGE_PAYE("Congé payé"),
+	/** The rtt. */
+	RTT("RTT"),
+	/** The conge sans solde. */
+	CONGE_SANS_SOLDE("Congé sans solde"),
+	/** The mission. */
+	MISSION("Mission");
 
+	/** The conge. */
 	private String conge;
 
+	/**
+	 * Instantiates a new conge enum.
+	 *
+	 * @param conge
+	 *            the conge
+	 */
 	private CongeEnum(String conge) {
 		this.conge = conge;
 	}
 
+	/**
+	 * Gets the conge enum.
+	 *
+	 * @return the conge enum
+	 */
 	public String getCongeEnum() {
 		return conge;
 	}
 
+	/**
+	 * Sets the conge enum.
+	 *
+	 * @param conge
+	 *            the new conge enum
+	 */
 	public void setCongeEnum(String conge) {
 		this.conge = conge;
 	}
